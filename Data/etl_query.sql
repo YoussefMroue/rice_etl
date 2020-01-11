e8ac6985a07890dd5afbc110555f5af8ae778f65
solicia <solicia@solicias-mbp.dyndns.rice.edu>
2020-01-11 12:26:57 -0600

    clean datasets and sql query

--- Data/etl_query.sql
+++ Data/etl_query.sql
@@ -1,31 +1,45 @@
 drop table birth_dates
+drop table currencies
+drop table currency
+drop table food_production
+
 
 create table birth_dates(
+	Countrycode varchar references currencies(country_code),
 	Country varchar,
-	Countrycode varchar,
 	Year numeric,
 	BirthRate decimal);
 
 select * from birth_dates;
 
+create table currencies(
+	Currency varchar unique,
+	Country varchar unique,
+	Country_Code varchar unique);
+
+select * from currencies;
+	
 create table currency(
-	Currency varchar,
+	Currency varchar references currencies(currency),
 	Date numeric,
 	Rate decimal);
 	
 select * from currency;
 
 create table food_production(
-	Country varchar,
+	Country varchar references currencies(country),
 	Countrycode varchar,
 	Year numeric,
 	FoodProduction decimal);
 	
 select * from food_production;
 
-create table currencies(
-	Currency varchar,
-	Country varchar,
-	Country_Code varchar);
-
-select * from currencies;
+SELECT birth_dates.country,birth_dates.year,birth_dates.birthrate,food_production.foodproduction,currencies.currency,currency.rate
+FROM currencies
+LEFT JOIN currency
+ON currency.currency = currencies.currency
+LEFT JOIN birth_dates
+ON currencies.country = birth_dates.country
+LEFT JOIN food_production
+ON currencies.country = food_production.country
+WHERE currencies.country_code = 'ARE'
