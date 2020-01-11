b9239e1cbf2382beb6bcd321394bb47af828fef6
Vaishall Pradeepkumar <mastervaishall@gmail.com>
2020-01-11 14:36:04 -0600

    Flask

--- Data/etl_query.sql
+++ Data/etl_query.sql
@@ -1,45 +1,27 @@
-drop table birth_dates
-drop table currencies
-drop table currency
-drop table food_production
-
-
-create table birth_dates(
-	Countrycode varchar references currencies(country_code),
-	Country varchar,
-	Year numeric,
-	BirthRate decimal);
-
-select * from birth_dates;
+drop table birth_rates;
+drop table currencies;
+drop table currency;
+drop table food_production;
 
 create table currencies(
+	id serial primary key,
 	Currency varchar unique,
 	Country varchar unique,
 	Country_Code varchar unique);
-
-select * from currencies;
-	
+create table birth_rates(
+	id serial primary key,
+	Countrycode varchar references currencies(country_code),
+	Country varchar,
+	Year int,
+	BirthRate float);
 create table currency(
+	id serial primary key,
 	Currency varchar references currencies(currency),
-	Date numeric,
-	Rate decimal);
-	
-select * from currency;
-
+	Date int,
+	Rate float);
 create table food_production(
+	id serial primary key,
 	Country varchar references currencies(country),
 	Countrycode varchar,
-	Year numeric,
-	FoodProduction decimal);
-	
-select * from food_production;
-
-SELECT birth_dates.country,birth_dates.year,birth_dates.birthrate,food_production.foodproduction,currencies.currency,currency.rate
-FROM currencies
-LEFT JOIN currency
-ON currency.currency = currencies.currency
-LEFT JOIN birth_dates
-ON currencies.country = birth_dates.country
-LEFT JOIN food_production
-ON currencies.country = food_production.country
-WHERE currencies.country_code = 'ARE'
+	Year int,
+	FoodProduction float);
